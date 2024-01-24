33accbd24ee7b32c2d5345fd210322719e7a7818
zoperalex <alex.michaelides17@gmail.com>
2024-01-24 13:46:40 +0200

    Created Player Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private float speed = 10;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        if (horizontalInput != 0 || verticalInput != 0)
+        {
+            Vector3 direction = (new Vector3(horizontalInput, verticalInput, 0)).normalized;
+            this.transform.position += direction * speed * Time.deltaTime;
+        }
     }
 }
